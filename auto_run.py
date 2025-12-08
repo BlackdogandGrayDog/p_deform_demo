5bfaae207a0213aa5452a43e6b437cfd1eaca06b
BlackdogandGrayDog <ericwei09@outlook.com>
2025-12-08 22:18:37 +0000

    123

--- auto_run.py
+++ auto_run.py
@@ -6,7 +6,7 @@ import signal
 # Define training parameter combinations
 trajectories = ['5']
 loss_models = ['patched_0.005_0.01']
-steps = ['150k']
+steps = ['300k']
 
 dataset_name = "hamlyn"
 finetune_dataset_name = dataset_name
